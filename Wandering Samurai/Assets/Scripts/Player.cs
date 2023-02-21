e68c3b1356323298f80eac50585cd4bedb48b178
wolfofRome <prideofguardian0607@gmail.com>
2023-02-21 22:01:41 +0100

    init
    Game added to GitHub

---
+++ Wandering Samurai/Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int HP = 20;
+    public float walking = 5.0f;
+    public float running = 7.0f;
+    public float jumpPower = 10.0f;
+    
+}
---
+++ Wandering Samurai/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal;
+    private float jumpingPower = 16f;
+    // private bool isFacingRight = true;
+
+    private Rigidbody2D rb;
+    private Player player;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    void Start()
+    {
+        player = GetComponent<Player>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isGrounded()) 
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        //Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        
+        rb.velocity = new Vector2(horizontal * getSpeed(), rb.velocity.y);
+    }
+
+    private float getSpeed()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            return player.running;
+        }
+        return player.walking;
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    /*private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }*/
+}
